b0357af7a82ab90bbe652e79644406e09782a2fb
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-05-09 18:30:25 +0900

    connectiong to db from php file

--- code/index.php
+++ code/index.php
@@ -1,3 +1,8 @@
 <?php
 //You can access this file by localhost:80
 echo "This is index.php";
+
+//connecting to db 
+$dsn = "mysql:host=mysql; charset=utf8; port=3306;";
+$connection = new PDO($dsn,"root","password1");
+
